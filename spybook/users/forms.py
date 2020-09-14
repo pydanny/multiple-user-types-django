fc35fad890ec659206572d0a6266a29c8aae5cc8
Daniel Feldroy <pydanny@users.noreply.github.com>
2020-09-14 12:03:38 -0700

    Update forms.py

--- spybook/users/forms.py
+++ spybook/users/forms.py
@@ -2,6 +2,10 @@ from django.contrib.auth import get_user_model, forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
+# Importing django.forms under a special namespace because of my old mistake
+from django import forms as d_forms
+from allauth.account.forms import SignupForm
+
 User = get_user_model()
 
 
@@ -34,3 +38,23 @@ class UserCreationForm(forms.UserCreationForm):
         raise ValidationError(
             self.error_messages["duplicate_username"]
         )
+        
+# SpyBookSignupForm inherits from django-allauth's SignupForm
+class SpyBookSignupForm(SignupForm):
+
+    # Specify a choice field that matches the choice field on our user model
+    type = d_forms.ChoiceField(choices=[("SPY", "Spy"), ("DRIVER", "Driver")])
+
+    # Override the init method
+    def __init__(self, *args, **kwargs):
+        # Call the init of the parent class
+        super().__init__(*args, **kwargs)
+        # Remove autofocus because it is in the wrong place
+        del self.fields["username"].widget.attrs["autofocus"]
+
+    # Put in custom signup logic
+    def custom_signup(self, request, user):
+        # Set the user's type from the form reponse
+        user.type = self.cleaned_data["type"]
+        # Save the user's type to their database record
+        user.save()        
